d926237767a02e60b7cc217a0832a380c5395973
jbeemster <joshua.a.beemster@gmail.com>
2020-12-21 14:31:44 +0100

    Make stdin multi-threaded

--- core/source_stdin.go
+++ core/source_stdin.go
@@ -11,6 +11,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/twinj/uuid"
 	"os"
+	"sync"
 )
 
 // StdinSource holds a new client for reading events from stdin
@@ -25,6 +26,9 @@ func NewStdinSource() (*StdinSource, error) {
 func (ss *StdinSource) Read(sf *SourceFunctions) error {
 	log.Infof("Reading records from 'stdin', scanning until EOF detected (Note: Press 'CTRL + D' to exit)")
 
+	throttle := make(chan struct{}, 10)
+	wg := sync.WaitGroup{}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		events := []*Event{
@@ -33,12 +37,19 @@ func (ss *StdinSource) Read(sf *SourceFunctions) error {
 				PartitionKey: uuid.NewV4().String(),
 			},
 		}
-		err := sf.WriteToTarget(events)
-		if err != nil {
-			log.Error(err)
-		}
-	}
 
+		throttle <- struct{}{}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := sf.WriteToTarget(events)
+			if err != nil {
+				log.Error(err)
+			}
+			<-throttle
+		}()
+	}
+	wg.Wait()
 	sf.CloseTarget()
 
 	if scanner.Err() != nil {
