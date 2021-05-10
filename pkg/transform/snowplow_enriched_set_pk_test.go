7c61cacebc0e5a087e8018c0a9eb7b4975284334
colmsnowplow <colm@snowplowanalytics.com>
2021-05-10 12:34:39 +0100

    Introduce IntermediateState to avoid parsing event multiple times

--- pkg/transform/snowplow_enriched_set_pk_test.go
+++ pkg/transform/snowplow_enriched_set_pk_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/snowplow-devops/stream-replicator/pkg/models"
+	"github.com/snowplow/snowplow-golang-analytics-sdk/analytics"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -56,4 +57,44 @@ func TestNewSpEnrichedSetPkFunction(t *testing.T) {
 	assert.NotNil(fail)
 	assert.Equal("Cannot parse tsv event - wrong number of fields provided: 20", fail.GetError().Error())
 	// Error message to be updated after fix in analytics sdk
+}
+
+func TestNewSpEnrichedSetPkFunction_WithIntermediateState(t *testing.T) {
+	assert := assert.New(t)
+
+	tsvEvent := []byte(`test-data	pc	2019-05-10 14:40:37.436	2019-05-10 14:40:35.972	2019-05-10 14:40:35.551	unstruct	e9234345-f042-46ad-b1aa-424464066a33			py-0.8.2	ssc-0.15.0-googlepubsub	beam-enrich-0.2.0-common-0.36.0	user<built-in function input>	18.194.133.57				d26822f5-52cc-4292-8f77-14ef6b7a27e2																																									{"schema":"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0","data":{"schema":"iglu:com.snowplowanalytics.snowplow/add_to_cart/jsonschema/1-0-0","data":{"sku":"item41","quantity":2,"unitPrice":32.4,"currency":"GBP"}}}																			python-requests/2.21.0																																										2019-05-10 14:40:35.000			{"schema":"iglu:com.snowplowanalytics.snowplow/contexts/jsonschema/1-0-1","data":[{"schema":"iglu:nl.basjes/yauaa_context/jsonschema/1-0-0","data":{"deviceBrand":"Unknown","deviceName":"Unknown","operatingSystemName":"Unknown","agentVersionMajor":"2","layoutEngineVersionMajor":"??","deviceClass":"Unknown","agentNameVersionMajor":"python-requests 2","operatingSystemClass":"Unknown","layoutEngineName":"Unknown","agentName":"python-requests","agentVersion":"2.21.0","layoutEngineClass":"Unknown","agentNameVersion":"python-requests 2.21.0","operatingSystemVersion":"??","agentClass":"Special","layoutEngineVersion":"??"}}]}		2019-05-10 14:40:35.972	com.snowplowanalytics.snowplow	add_to_cart	jsonschema	1-0-0		`)
+	parsed, _ := analytics.ParseEvent(string(tsvEvent))
+
+	message := models.Message{
+		Data:              tsvEvent,
+		PartitionKey:      "some-key",
+		IntermediateState: parsed,
+	}
+
+	expected := models.Message{
+		Data:              tsvEvent,
+		PartitionKey:      "some-key",
+		IntermediateState: parsed,
+	}
+
+	aidSetPkFunc := NewSpEnrichedSetPkFunction("app_id")
+
+	stringAsPk, fail := aidSetPkFunc(&message)
+
+	assert.Equal("test-data", stringAsPk.PartitionKey)
+	assert.Equal(expected.Data, stringAsPk.Data)
+	assert.Equal(expected.IntermediateState, stringAsPk.IntermediateState)
+	assert.Nil(fail)
+
+	incompatibleIntermediateMessage := models.Message{
+		Data:              tsvEvent,
+		PartitionKey:      "some-key",
+		IntermediateState: "Incompatible intermediate state",
+	}
+
+	stringAsPkIncompat, failIncompat := aidSetPkFunc(&incompatibleIntermediateMessage)
+	assert.Equal("test-data", stringAsPkIncompat.PartitionKey)
+	assert.Equal(expected.Data, stringAsPkIncompat.Data)
+	assert.Equal(expected.IntermediateState, stringAsPkIncompat.IntermediateState)
+	assert.Nil(failIncompat)
 }
