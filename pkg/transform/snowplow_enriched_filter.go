3b10912567a4d88e8a279951090ed365b6c84fa9
colmsnowplow <colm@snowplowanalytics.com>
2021-07-13 12:12:46 +0100

    Improve readability of filtering logic

--- pkg/transform/snowplow_enriched_filter.go
+++ pkg/transform/snowplow_enriched_filter.go
@@ -33,20 +33,20 @@ func NewSpEnrichedFilterFunction(filterConfig string) (TransformationFunction, e
 	// Check for a negation condition first
 	keyValues := strings.SplitN(filterConfig, "!=", 2)
 
-	// Initial Keep Value is the t/f value of keepMessage, to be reset on every invocation of the returned function
-	var initialKeepValue bool
+	// isNegationFilter determines whether a match sets shouldKeepMessage to true or false, and consequently whether message is kept or filtered
+	var isNegationFilter bool
 	if len(keyValues) > 1 {
 		// If negation condition is found, default to keep the message, and change this when match found
-		initialKeepValue = true
+		isNegationFilter = true
 	} else {
 		// Otherwise, look for affirmation condition, default to drop the message and change when match found
 		keyValues = strings.SplitN(filterConfig, "==", 2)
-		initialKeepValue = false
+		isNegationFilter = false
 	}
 
 	return func(message *models.Message, intermediateState interface{}) (*models.Message, *models.Message, *models.Message, interface{}) {
-		// Start by resetting keepMessage to initialKeepValue
-		keepMessage := initialKeepValue
+		// Start by resetting shouldKeepMessage to isNegationFilter
+		shouldKeepMessage := isNegationFilter
 
 		// Evalute intermediateState to parsedEvent
 		parsedMessage, parseErr := intermediateAsParsed(intermediateState, message)
@@ -68,14 +68,18 @@ func NewSpEnrichedFilterFunction(filterConfig string) (TransformationFunction, e
 	evaluation:
 		for _, valueToMatch := range strings.Split(keyValues[1], "|") {
 			if valueToMatch == fmt.Sprintf("%v", valueFound) { // coerce to string as valueFound may be any type found in a Snowplow event
-				keepMessage = !keepMessage
+				if isNegationFilter {
+					shouldKeepMessage = false
+				} else {
+					shouldKeepMessage = true
+				}
 				break evaluation
-				// Once config value is matched once, change keepMessage then break out of the loop to avoid reverting back when we have two matches
+				// Once config value is matched once, change shouldKeepMessage, and stop looking for matches
 			}
 		}
 
 		// If message is not to be kept, return it as a filtered message to be acked in the main function
-		if !keepMessage {
+		if !shouldKeepMessage {
 
 			return nil, message, nil, nil
 		}
