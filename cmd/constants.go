e063715047197b38f5a784363b13eca7d78c03b2
colmsnowplow <colm@snowplowanalytics.com>
2021-07-26 19:00:34 +0100

    Bump to rc3

--- cmd/constants.go
+++ cmd/constants.go
@@ -8,7 +8,7 @@ package cmd
 
 const (
 	// AppVersion is the current version of the replicator
-	AppVersion = "0.5.0-rc2"
+	AppVersion = "0.5.0-rc3"
 
 	// AppName is the name of the application to use in logging / places that require the artifact
 	AppName = "stream-replicator"
