ebbf2f462f0195ee952874d2013f1e40fcc00f6a
colmsnowplow <colm@snowplowanalytics.com>
2021-08-17 16:33:14 +0100

    Cleanup kinesis source additional logging

--- pkg/source/kinesis.go
+++ pkg/source/kinesis.go
@@ -140,11 +140,9 @@ func (ks *KinesisSource) Read(sf *sourceiface.SourceFunctions) error {
 				<-throttle
 			}()
 		} else {
-			ks.log.Info("BETA TEST DEBUG: Returning nil: record = nil")
 			return nil
 		}
 	}
-	ks.log.Info("BETA TEST DEBUG: Unreachable code reached somehow.")
 	wg.Wait()
 
 	return nil
