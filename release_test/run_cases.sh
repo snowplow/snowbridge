9a25b2a0432cd513b74ddb251a9c46c16adba46c
colmsnowplow <colm@snowplowanalytics.com>
2022-09-19 16:54:15 +0100

    Use docker command for tests

--- release_test/run_cases.sh
+++ release_test/run_cases.sh
@@ -11,15 +11,32 @@ for casepath in ${SCRIPT_DIR}/cases/*/ ; do
 
     # Iterate through the test cases and run the SR binary for each of them
     echo $casepath
-    export STREAM_REPLICATOR_CONFIG_FILE="${casepath}config.hcl"
+    # export STREAM_REPLICATOR_CONFIG_FILE="${casepath}config.hcl"
 
-    cat ${casepath}input.txt | ${SCRIPT_DIR}/stream-replicator > ${casepath}result.txt
+    # cat ${casepath}input.txt | ${SCRIPT_DIR}/stream-replicator > ${casepath}result.txt
+    
+    # -i keeps stdin open
+    # --mount mounts the config
+    # --env sets the config env var
+    cat ${casepath}input.txt | docker run -i \
+        --mount type=bind,source=${casepath}config.hcl,target=/config.hcl \
+        --env STREAM_REPLICATOR_CONFIG_FILE=/config.hcl \
+        snowplow/stream-replicator-aws:1.0.0 \
+    > ${casepath}result.txt # output to file
+
+
+    # The docker way is something like:
+    # cat cases/spEnrichedFilter/input.txt | docker run -i --mount type=bind,source="$(pwd)"/cases/spEnrichedFilter/config.hcl,target=/config.hcl --env STREAM_REPLICATOR_CONFIG_FILE=/config.hcl snowplow/stream-replicator-aws:1.0.0 > result.txt
+
+    # `make container` first
+    # Then we just need management of the version number. Could do that with cat VERSION...
 
     # run test for each case
     go test $casepath
+    
 done
 
-# could put go test ${SCRIPT_DIR}/... here too if we wanted to do it that way.
+# TODO:
 
-# We may also just want one directory of input files, rather than having them all in the different `cases` folders.
-# For a quick spike of the tests though, this is grand.
+# For now, running each test as part of this script.. Might be better to do that as its own GH action though.
+# Also need to run it once each for gcp and aws
