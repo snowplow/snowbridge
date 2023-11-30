4c7a885738a6a16c0017dd57ff65ed9eb9e08903
spenes <enes@snowplowanalytics.com>
2023-11-30 17:49:51 +0300

    2.2.2-rc2

--- cmd/constants.go
+++ cmd/constants.go
@@ -9,7 +9,7 @@ package cmd
 
 const (
 	// AppVersion is the current version of the app
-	AppVersion = "2.2.2"
+	AppVersion = "2.2.2-rc2"
 
 	// AppName is the name of the application to use in logging / places that require the artifact
 	AppName = "snowbridge"
