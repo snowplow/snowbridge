cae6f759e28785a602b31b5205f6b4c10d5887e7
colmsnowplow <colm@snowplowanalytics.com>
2024-05-03 17:26:13 +0100

    Patch minor test issues (close #322)

--- release_test/e2e_transform_test.go
+++ release_test/e2e_transform_test.go
@@ -111,7 +111,7 @@ func evaluateTestCasePK(t *testing.T, actual []byte, expectedFilePath string, te
 	sort.Strings(foundData)
 	sort.Strings(expectedData)
 
-	assert.Equal(expectedData, foundData)
+	assert.Equal(expectedData, foundData, testCase)
 }
 
 func getFileMountArg(testCase string) string {
