d6ba802ffb192e0dcedc08a49c41a0dc05a0aa81
colmsnowplow <colm@snowplowanalytics.com>
2024-11-15 18:12:19 +0000

    Add configuration settings for PubSub source (#380)
    Co-authored-by: adatzer <ada@snowplowanalytics.com>

--- pkg/source/pubsub/pubsub_source.go
+++ pkg/source/pubsub/pubsub_source.go
@@ -20,6 +20,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/api/option"
 
 	"github.com/snowplow/snowbridge/config"
 	"github.com/snowplow/snowbridge/pkg/models"
@@ -28,17 +29,25 @@ import (
 
 // Configuration configures the source for records pulled
 type Configuration struct {
-	ProjectID        string `hcl:"project_id"`
-	SubscriptionID   string `hcl:"subscription_id"`
-	ConcurrentWrites int    `hcl:"concurrent_writes,optional"`
+	ProjectID                 string `hcl:"project_id"`
+	SubscriptionID            string `hcl:"subscription_id"`
+	ConcurrentWrites          int    `hcl:"concurrent_writes,optional"`
+	MaxOutstandingMessages    int    `hcl:"max_outstanding_messages,optional"`
+	MaxOutstandingBytes       int    `hcl:"max_outstanding_bytes,optional"`
+	MinExtensionPeriodSeconds int    `hcl:"min_extension_period_seconds,optional"`
+	StreamingPullGoRoutines   int    `hcl:"streaming_pull_goroutines,optional"`
+	GRPCConnectionPool        int    `hcl:"grpc_connection_pool_size,optional"`
 }
 
 // pubSubSource holds a new client for reading messages from PubSub
 type pubSubSource struct {
-	projectID        string
-	client           *pubsub.Client
-	subscriptionID   string
-	concurrentWrites int
+	projectID                 string
+	client                    *pubsub.Client
+	subscriptionID            string
+	maxOutstandingMessages    int
+	maxOutstandingBytes       int
+	minExtensionPeriodSeconds int
+	streamingPullGoRoutines   int
 
 	log *log.Entry
 
@@ -52,6 +61,11 @@ func configFunction(c *Configuration) (sourceiface.Source, error) {
 		c.ConcurrentWrites,
 		c.ProjectID,
 		c.SubscriptionID,
+		c.MaxOutstandingMessages,
+		c.MaxOutstandingBytes,
+		c.MinExtensionPeriodSeconds,
+		c.StreamingPullGoRoutines,
+		c.GRPCConnectionPool,
 	)
 }
 
@@ -68,7 +82,12 @@ func (f adapter) Create(i interface{}) (interface{}, error) {
 func (f adapter) ProvideDefault() (interface{}, error) {
 	// Provide defaults
 	cfg := &Configuration{
-		ConcurrentWrites: 50,
+		// ConcurrentWrites:          50,
+		// Default is now handled in newPubsubSource, until we make a breaking release.
+		MaxOutstandingMessages: 1000,
+		MaxOutstandingBytes:    1e9,
+		// StreamingPullGoRoutines:   1,
+		// Similarly handled in newPubsubSource - when we make a breaking release this should be the default.
 	}
 
 	return cfg, nil
@@ -93,23 +112,52 @@ var ConfigPair = config.ConfigurationPair{
 }
 
 // newPubSubSource creates a new client for reading messages from PubSub
-func newPubSubSource(concurrentWrites int, projectID string, subscriptionID string) (*pubSubSource, error) {
+func newPubSubSource(concurrentWrites int, projectID string, subscriptionID string, maxOutstandingMessages, maxOutstandingBytes int, minExtensionPeriodSeconds int, streamingPullGoRoutines int, grpcConnectionPool int) (*pubSubSource, error) {
 	ctx := context.Background()
 
 	// Ensures as even as possible distribution of UUIDs
 	uuid.EnableRandPool()
 
-	client, err := pubsub.NewClient(ctx, projectID)
+	log := log.WithFields(log.Fields{"source": "pubsub", "cloud": "GCP", "project": projectID, "subscription": subscriptionID})
+
+	// We use a slice to provide the grpcConnectionPool option only if it is set.
+	// Otherwise we'll overwrite the client's clever under-the-hood default behaviour:
+	// https://github.com/googleapis/google-cloud-go/blob/380e7d23e69b22ab46cc6e3be58902accee2f26a/pubsub/pubsub.go#L165-L177
+	var opt []option.ClientOption
+	if grpcConnectionPool != 0 {
+		opt = append(opt, option.WithGRPCConnectionPool(grpcConnectionPool))
+	}
+
+	client, err := pubsub.NewClient(ctx, projectID, opt...)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to create PubSub client")
 	}
 
+	// This temproary logic allows us to fix suboptimal behaviour without a breaking release.
+	// The order of priority is streaming_pull_goroutines > concurrent_writes > previous default
+	// We don't change the default becuase this would cause a major behaviour change in a non-major version bump
+
+	// If streamingPullGoRoutines is not set but concurrentWrites is, use concurrentWrites.
+	if streamingPullGoRoutines == 0 && concurrentWrites != 0 {
+		streamingPullGoRoutines = concurrentWrites
+		log.Warn("For the pubsub source, concurrent_writes is deprecated, and will be removed in the next major version. Use streaming_pull_goroutines instead")
+	}
+	// If neither are set, set it to the new defult, but warn users of this behaviour change
+	if streamingPullGoRoutines == 0 && concurrentWrites == 0 {
+		streamingPullGoRoutines = 50
+		log.Warn("Neither streaming_pull_goroutines nor concurrent_writes are set. The previous default is preserved, but strongly advise manual configuration of streaming_pull_goroutines, max_outstanding_messages and max_outstanding_bytes")
+	}
+	// Otherwise, streamingPullGoRoutines is set in the config and that value will be used.
+
 	return &pubSubSource{
-		projectID:        projectID,
-		client:           client,
-		subscriptionID:   subscriptionID,
-		concurrentWrites: concurrentWrites,
-		log:              log.WithFields(log.Fields{"source": "pubsub", "cloud": "GCP", "project": projectID, "subscription": subscriptionID}),
+		projectID:                 projectID,
+		client:                    client,
+		subscriptionID:            subscriptionID,
+		maxOutstandingMessages:    maxOutstandingMessages,
+		maxOutstandingBytes:       maxOutstandingBytes,
+		minExtensionPeriodSeconds: minExtensionPeriodSeconds,
+		streamingPullGoRoutines:   streamingPullGoRoutines,
+		log:                       log,
 	}, nil
 }
 
@@ -120,7 +168,10 @@ func (ps *pubSubSource) Read(sf *sourceiface.SourceFunctions) error {
 	ps.log.Info("Reading messages from subscription ...")
 
 	sub := ps.client.Subscription(ps.subscriptionID)
-	sub.ReceiveSettings.NumGoroutines = ps.concurrentWrites
+	sub.ReceiveSettings.NumGoroutines = ps.streamingPullGoRoutines         // This sets the number of goroutines that can open a streaming pull at once
+	sub.ReceiveSettings.MaxOutstandingMessages = ps.maxOutstandingMessages // maxOutstandingMessages limits the number of messages processed at once (each spawns a goroutine)
+	sub.ReceiveSettings.MaxOutstandingBytes = ps.maxOutstandingBytes
+	sub.ReceiveSettings.MinExtensionPeriod = time.Duration(ps.minExtensionPeriodSeconds) * time.Second
 
 	cctx, cancel := context.WithCancel(ctx)
 
