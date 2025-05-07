f5495b4d297f5176b4376c4323ff9fd60a215684
colmsnowplow <colm@snowplowanalytics.com>
2025-05-07 16:39:56 +0100

    Add missing licence notices

--- pkg/transform/util.go
+++ pkg/transform/util.go
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2020-present Snowplow Analytics Ltd.
+ * All rights reserved.
+ *
+ * This software is made available by Snowplow Analytics, Ltd.,
+ * under the terms of the Snowplow Limited Use License Agreement, Version 1.1
+ * located at https://docs.snowplow.io/limited-use-license-1.1
+ * BY INSTALLING, DOWNLOADING, ACCESSING, USING OR DISTRIBUTING ANY PORTION
+ * OF THE SOFTWARE, YOU AGREE TO THE TERMS OF SUCH LICENSE AGREEMENT.
+ */
+
 package transform
 
 // RemoveNullFields removes null fields, empty maps and empty slices from the input object, as long as map keys are strings.
