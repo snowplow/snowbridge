6400510286cba47ab1a3b819ca07e2ae97cf11c1
oo-sleeper <mikhail.molotkov@snowplowanalytics.com>
2025-05-12 19:05:24 +0100

    fixing tests

--- pkg/common/helpers_test.go
+++ pkg/common/helpers_test.go
@@ -45,12 +45,12 @@ func TestGetAWSConfig(t *testing.T) {
 	t.Setenv("AWS_SHARED_CREDENTIALS_FILE", "")
 	cfg, accID, err := GetAWSConfig("us-east-1", "")
 	assert.Nil(cfg)
-	assert.Nil(accID)
+	assert.Equal("", accID)
 	assert.NotNil(err)
 
 	cfg2, accID2, err2 := GetAWSConfig("us-east-1", "some-role-arn")
 	assert.NotNil(cfg2)
-	assert.Nil(accID2)
+	assert.Equal("", accID2)
 	assert.NotNil(err2)
 	if err != nil {
 		assert.Equal("InvalidParameter: 1 validation error(s) found.\n- minimum field size of 20, AssumeRoleInput.RoleArn.\n", err2.Error())
