eeacbf2d2f7d911e0435cc865b66376f287f6026
colmsnowplow <colm@snowplowanalytics.com>
2025-07-23 12:28:04 +0100

    Move function and add comment

--- third_party/snowplow/badrows/bad_row.go
+++ third_party/snowplow/badrows/bad_row.go
@@ -45,6 +45,43 @@ type BadRow struct {
 	selfDescribingData *iglu.SelfDescribingData
 }
 
+// newBadRow returns a new bad-row structure
+func newBadRow(schema string, data map[string]any, payload []byte, targetByteLimit int) (*BadRow, error) {
+	payloadLength := len(payload)
+
+	// Ensure data map does not contain anything for payload
+	data[dataKeyPayload] = map[string]any{}
+
+	// Check bytes allocated to data map (without payload)
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not unmarshall bad-row data blob to JSON")
+	}
+	currentByteCount := len(schema) + badRowWrapperBytes + len(dataBytes)
+
+	// Figure out if we have enough bytes left to include the payload (or a truncated payload)
+	bytesForPayload := targetByteLimit - currentByteCount
+	if bytesForPayload <= 0 {
+		return nil, errors.New("Failed to create bad-row as resultant payload will exceed the targets byte limit")
+	}
+
+	// Add the payload into the data map
+	if payloadLength > bytesForPayload {
+		data[dataKeyPayload] = string(payload[:bytesForPayload])
+	} else {
+		data[dataKeyPayload] = string(payload)
+	}
+
+	return &BadRow{
+		schema: schema,
+		selfDescribingData: iglu.NewSelfDescribingData(
+			schema,
+			data,
+		),
+	}, nil
+}
+
+// newBadRowEventForwardingError does the same thing, but allows for the more complex payloads in this bad row type
 func newBadRowEventForwardingError(schema string, data map[string]any, payload map[string]string, targetByteLimit int) (*BadRow, error) {
 
 	originalTSV := []byte(payload[dataKeyOriginalTSV])
@@ -96,42 +133,6 @@ func newBadRowEventForwardingError(schema string, data map[string]any, payload m
 	}
 
 	data[dataKeyPayload] = payloadMap
-
-	return &BadRow{
-		schema: schema,
-		selfDescribingData: iglu.NewSelfDescribingData(
-			schema,
-			data,
-		),
-	}, nil
-}
-
-// newBadRow returns a new bad-row structure
-func newBadRow(schema string, data map[string]any, payload []byte, targetByteLimit int) (*BadRow, error) {
-	payloadLength := len(payload)
-
-	// Ensure data map does not contain anything for payload
-	data[dataKeyPayload] = map[string]any{}
-
-	// Check bytes allocated to data map (without payload)
-	dataBytes, err := json.Marshal(data)
-	if err != nil {
-		return nil, errors.Wrap(err, "Could not unmarshall bad-row data blob to JSON")
-	}
-	currentByteCount := len(schema) + badRowWrapperBytes + len(dataBytes)
-
-	// Figure out if we have enough bytes left to include the payload (or a truncated payload)
-	bytesForPayload := targetByteLimit - currentByteCount
-	if bytesForPayload <= 0 {
-		return nil, errors.New("Failed to create bad-row as resultant payload will exceed the targets byte limit")
-	}
-
-	// Add the payload into the data map
-	if payloadLength > bytesForPayload {
-		data[dataKeyPayload] = string(payload[:bytesForPayload])
-	} else {
-		data[dataKeyPayload] = string(payload)
-	}
 
 	return &BadRow{
 		schema: schema,
